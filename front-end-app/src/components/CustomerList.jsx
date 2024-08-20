db0d6d201cd3d406d92b9f45e8beb51686a76a56
Ryan Quiogue <rquiogue17@gmail.com>
2024-08-20 16:17:01 -0400

    Updated so that values would change properly

--- front-end-app/src/components/CustomerList.jsx
+++ front-end-app/src/components/CustomerList.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { getAll } from '../restdb';
 
 const CustomerList = (props) => {
   const {
@@ -40,6 +41,10 @@ const CustomerList = (props) => {
   const clearSearch = () => {
     setSearchText('');
   }
+
+  useEffect(() => {
+    getAll();
+  });
 
   return (
     <div className='container'>
