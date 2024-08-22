6cb40f23b496bf0453f3f22c6df90938522b9ff6
Your Name <you@example.com>
2024-08-22 10:47:36 -0400

    Got rid of Security folder

--- Security/src/main/java/com/webage/repository/CustomersRepository.java
+++
@@ -1,8 +0,0 @@
-package com.webage.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.webage.Customers.Customer;
-
-public interface CustomersRepository extends CrudRepository<Customer, Long> {
-}
